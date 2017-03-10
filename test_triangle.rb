075d58193844d4097c4034d32bfda833a30dd021
Betty Baker <betty@heysparkbox.com>
2017-03-10 15:30:26 -0500

    :bug: fix: Instantiate class Triangle on method test_greater_than_zero

--- test_triangle.rb
+++ test_triangle.rb
@@ -3,6 +3,6 @@ require "./triangle.rb"
 
 class TestTriangle < Minitest::Test
   def test_greater_than_zero
-    assert_equal true, Triangle.greater_than_zero
+    assert_equal true, Triangle.new.greater_than_zero
   end
 end
