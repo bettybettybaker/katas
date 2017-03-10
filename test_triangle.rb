81a6f184269a644c4c3f28916a3fe0cae0300416
Betty Baker <betty@heysparkbox.com>
2017-03-10 15:02:30 -0500

    :bug: fix: Made class TestTriangle and subclass of Minitest::Test

--- test_triangle.rb
+++ test_triangle.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "./triangle.rb"
 
-class TestTriangle
+class TestTriangle < Minitest::Test
   def test_all_sides_greater_than_zero
     assert_equal true, Triangle
   end
