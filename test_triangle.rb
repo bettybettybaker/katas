debcf7b04d48f99eb5bfe254110915afad201d29
Betty Baker <betty@heysparkbox.com>
2017-03-10 15:18:14 -0500

    :art: style: Rename method to test_greater_than_zero

--- test_triangle.rb
+++ test_triangle.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require "./triangle.rb"
 
 class TestTriangle < Minitest::Test
-  def test_all_sides_greater_than_zero
+  def test_greater_than_zero
     assert_equal true, Triangle.all_sides_greater_than_zero
   end
 end
