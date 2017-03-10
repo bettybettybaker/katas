a059c849daf7d739bc6184fc582f44bafc923c8d
Betty Baker <betty@heysparkbox.com>
2017-03-10 15:12:33 -0500

    :racehorse: test: Expand on def test_all_sides_greater_than_zero

--- test_triangle.rb
+++ test_triangle.rb
@@ -3,6 +3,6 @@ require "./triangle.rb"
 
 class TestTriangle < Minitest::Test
   def test_all_sides_greater_than_zero
-    assert_equal true, Triangle
+    assert_equal true, Triangle.all_sides_greater_than_zero
   end
 end
