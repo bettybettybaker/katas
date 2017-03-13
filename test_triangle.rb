81c2ccf64d57c3abf1c979deeb969d28ed8d2b53
Betty Baker <betty@heysparkbox.com>
2017-03-13 13:30:22 -0400

    :racehorse: feat: A new approach with TDD - this runs

--- test_triangle.rb
+++ test_triangle.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require "./triangle.rb"
+require "./triangle"
 
 =begin
 create more tests for each type of triangle. Work on being able to have an
@@ -8,7 +8,12 @@ blueprints and I need to tell them what kind they are
 =end
 
 class TestTriangle < Minitest::Test
-  def test_greater_than_zero
-    assert_equal true, Triangle.new(2,2,3).greater_than_zero
+  def test_triangle
+    valid_triangle = Triangle.new(2, 3, 4)
+    assert valid_triangle.valid?
   end
 end
+
+#  def test_does_triangle_exist
+#    assert_equal true, Triangle.new(2,2,3).greater_than_zero
+#  end
--- triangle.rb
+++ triangle.rb
@@ -1,39 +1,10 @@
-# https://github.com/sparkbox/apprenticeships/blob/overhaul/basic-programming/project-triangle-kata.md
-
-=begin
-
-# Fist attempt (did not use TDD)
-
-def triangle(a, b, c)
-  if ((a == b) && (a == c) && (b == c))
-    puts "equilateral"
-  elsif ((a == b) || (a == c) || (b == c))
-    puts "isosceles"
-  else
-    puts "scalene"
-  end
-end
-
-a = 1
-b = 2
-c = 3
-
-=end
-
-# Second attempt using TDD
-
 class Triangle
-  def greater_than_zero(:side1, :side2, :side3)
-    if :side1 * :side2 * :side3 = 0
-      return true
-    end
+  attr_reader :sides
+  def initialize(a, b, c)
+    @sides = [a, b, c]
   end
 
-  def sides
-    {
-      side1: 2,
-      side2: 2,
-      side3: 3
-    }
+  def valid?
+    sides[0] * sides[1] * sides[2] != 0
   end
 end
