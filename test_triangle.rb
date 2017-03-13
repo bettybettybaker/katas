f1afb3dd7a49cf8b18d65e7c087377962a186e2b
Betty Baker <betty@heysparkbox.com>
2017-03-13 12:03:16 -0400

    :racehorse: refactor: Building test. Committing then starting over

--- test_triangle.rb
+++ test_triangle.rb
@@ -1,8 +1,14 @@
 require "minitest/autorun"
 require "./triangle.rb"
 
+=begin
+create more tests for each type of triangle. Work on being able to have an
+instance of a class know what it is already when it is called. Classes are
+blueprints and I need to tell them what kind they are
+=end
+
 class TestTriangle < Minitest::Test
   def test_greater_than_zero
-    assert_equal true, Triangle.new.greater_than_zero
+    assert_equal true, Triangle.new(2,2,3).greater_than_zero
   end
 end
