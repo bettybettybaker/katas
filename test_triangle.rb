4971f8d08e36c87cc84fb919f4ddc4964ad24da4
Betty Baker <betty@heysparkbox.com>
2017-03-14 14:03:48 -0400

    SOLVED: Triangle Kata part 1

--- test_triangle.rb
+++ test_triangle.rb
@@ -3,12 +3,6 @@
 require "minitest/autorun"
 require "./triangle"
 
-=begin
-create more tests for each type of triangle. Work on being able to have an
-instance of a class know what it is already when it is called. Classes are
-blueprints and I need to tell them what kind they are
-=end
-
 class TestTriangle < Minitest::Test
 
   def test_negative_triangle
@@ -48,7 +42,3 @@ class TestTriangle < Minitest::Test
   end
 
 end
-
-#  def test_does_triangle_exist
-#    assert_equal true, Triangle.new(2,2,3).greater_than_zero
-#  end
