bc8882b4e6fe5dcf2974605c4161cedc1a753d34
Betty Baker <betty@heysparkbox.com>
2017-03-22 14:09:56 -0400

    :racehorse: refactor: Remove unnessasary 'else return false' from triangle functions

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,41 +1,52 @@
-const assert = require('assert');
+const assert = require("assert");
 // Equilateral
 
 function equilateral (sides){
   if (sides[0] === sides[1] && sides[1] === sides[2]) {
     return true;
-  } else {
-    return false;
-    }
+  }
 }
 
 // equilateral tests
 
 // true
-assert.deepEqual(true, equilateral([1, 1, 1]), "Sorry, testing [1, 1, 1] for an equilateral triangle returned false.");
+
+assert.deepEqual(
+  true, equilateral([1, 1, 1]),
+  "Sorry, testing [1, 1, 1] for an equilateral triangle returned false.");
 
 // false
-// assert.deepEqual(true, equilateral([1, 2, 3]), "Sorry, testing [1, 2, 3] for an equilateral triangle returned false.");
+assert.deepEqual(
+  true, equilateral([1, 2, 3]),
+  "Sorry, testing [1, 2, 3] for an equilateral triangle returned false.");
 
 // Isosceles
 
 function isosceles (sides){
   if (sides[0] === sides[0] || sides[1] === sides[2] || sides[2] === sides[0]){
     return true;
-} else {
-    return false;
   }
 }
 
 // isosceles tests
 
 //true
-assert.deepEqual(true, isosceles([1, 3, 3]), "Sorry, testing [1, 3, 3] for an isosceles triangle returned false.");
-assert.deepEqual(true, isosceles([2, 2, 5]), "Sorry, testing [2, 2, 5] for an isosceles triangle returned false.");
-assert.deepEqual(true, isosceles([1, 2, 3]), "Sorry, testing [1, 2, 3] for an isosceles triangle returned false.");
+assert.deepEqual(
+  true, isosceles([1, 3, 3]),
+  "Sorry, testing [1, 3, 3] for an isosceles triangle returned false.");
+
+assert.deepEqual(
+  true, isosceles([2, 2, 5]),
+  "Sorry, testing [2, 2, 5] for an isosceles triangle returned false.");
+
+assert.deepEqual(
+  true, isosceles([1, 2, 3]),
+  "Sorry, testing [1, 2, 3] for an isosceles triangle returned false.");
 
 // false
-// assert.deepEqual(true, isosceles([99, 80, 99), "Sorry, testing [99, 80, 99] for an isosceles triangle returned false.");
+assert.deepEqual(
+  true, isosceles([99, 80, 99]),
+  "Sorry, testing [99, 80, 99] for an isosceles triangle returned false.");
 
 
 // Scalene
@@ -43,16 +54,21 @@ assert.deepEqual(true, isosceles([1, 2, 3]), "Sorry, testing [1, 2, 3] for an is
 function scalene (sides){
   if (sides[0] != sides[1] && sides[1] != sides[2] && sides[2] != sides[0]){
     return true;
-} else {
-    return false;
   }
 }
 
 // scalene tests
 
 // true
-assert.deepEqual(true, scalene([1, 2, 3]), "Sorry, testing [1, 2, 3] for an scalene triangle returned false.");
+assert.deepEqual(
+  true, scalene([1, 2, 3]),
+  "Sorry, testing [1, 2, 3] for an scalene triangle returned false.");
 
 // false
-// assert.deepEqual(true, scalene([2, 2, 2]), "Sorry, testing [2, 2, 2] for an scalene triangle returned false.");
-// assert.deepEqual(true, scalene([1, 3, 3]), "Sorry, testing [1, 3, 3] for an scalene triangle returned false.");
+// assert.deepEqual(
+// true, scalene([2, 2, 2]),
+// "Sorry, testing [2, 2, 2] for an scalene triangle returned false.");
+
+// assert.deepEqual(
+// true, scalene([1, 3, 3]),
+// "Sorry, testing [1, 3, 3] for an scalene triangle returned false.");
