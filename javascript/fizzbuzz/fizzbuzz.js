b95635c77c7bf880547cd88dcf699823e508c3c3
Betty Baker <betty@heysparkbox.com>
2017-03-27 10:23:15 -0400

    :racehorse: refactor: For Loop

--- javascript/fizzbuzz/fizzbuzz.js
+++ javascript/fizzbuzz/fizzbuzz.js
@@ -1,9 +1,9 @@
 /*eslint no-console: ["off"] */
 
-let num = 0;
-
-for (num = 0; num <= 100; num += 1){
-  if (num % 3 == 0){
+for (let num = 1; num <= 100; num += 1) {
+  if (num % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0) {
     console.log("Fizz");
   } else if (num % 5 == 0) {
     console.log("Buzz");
