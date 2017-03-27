fc244c1d6558bb7e7fe09236fc7c2c8b181f3d3d
Betty Baker <betty@heysparkbox.com>
2017-03-27 11:05:30 -0400

    :racehorse: refactor: Put while loop into a function

--- javascript/fizzbuzz/fizzbuzz.js
+++ javascript/fizzbuzz/fizzbuzz.js
@@ -1,22 +1,26 @@
 /*eslint no-console: ["off"] */
 
+
 // let num = 0;
-
-// while (num <= 100){
-//   if (num % 15 == 0) {
-//     console.log("FizzBuzz");
-//     } else if (num % 3 == 0) {
-//     console.log("Fizz");
-//     } else if (num % 5 == 0) {
-//     console.log("Buzz");
-//     } else {
-//     console.log(num);
-//   }
-// num += 1;
-// }
-
-for (let num = 1; num <= 100; num += 1) {
-  var buzz = num % 5 == 0,
-      fizz = num % 3 == 0;
-  console.log(fizz ? buzz ? "FizzBuzz" : "Fizz" : buzz ? "Buzz" : num);
+function fizzBuzz (num) {
+  while (num <= 100){
+    if (num % 15 == 0) {
+      console.log("FizzBuzz");
+      } else if (num % 3 == 0) {
+      console.log("Fizz");
+      } else if (num % 5 == 0) {
+      console.log("Buzz");
+      } else {
+      console.log(num);
+    }
+  num += 1;
+  }
 }
+
+fizzBuzz(1);
+
+// for (let num = 1; num <= 100; num += 1) {
+//   var buzz = num % 5 == 0,
+//       fizz = num % 3 == 0;
+//   console.log(fizz ? buzz ? "FizzBuzz" : "Fizz" : buzz ? "Buzz" : num);
+// }
