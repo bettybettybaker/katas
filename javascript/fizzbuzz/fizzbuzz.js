b9333e216fded6e7cf47db430966e7763202718d
Betty Baker <betty@heysparkbox.com>
2017-03-27 10:44:04 -0400

    :racehorse: refactor: Use a while loop

--- javascript/fizzbuzz/fizzbuzz.js
+++ javascript/fizzbuzz/fizzbuzz.js
@@ -1,13 +1,28 @@
 /*eslint no-console: ["off"] */
 
-for (let num = 1; num <= 100; num += 1) {
+let num = 0;
+
+while (num <= 100){
   if (num % 15 == 0) {
     console.log("FizzBuzz");
-  } else if (num % 3 == 0) {
+    } else if (num % 3 == 0) {
     console.log("Fizz");
-  } else if (num % 5 == 0) {
+    } else if (num % 5 == 0) {
     console.log("Buzz");
-  } else {
+    } else {
     console.log(num);
   }
+num += 1;
 }
+
+// for (let num = 1; num <= 100; num += 1) {
+//   if (num % 15 == 0) {
+//     console.log("FizzBuzz");
+//   } else if (num % 3 == 0) {
+//     console.log("Fizz");
+//   } else if (num % 5 == 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(num);
+//   }
+// }
