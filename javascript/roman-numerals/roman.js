5cc85bc35a35d6dd08bfbd4c333ab2ba1f7b2428
Betty Baker <betty@heysparkbox.com>
2017-03-28 09:51:19 -0400

    :racehorse: feat: 1-10 in an if elseif statement

--- javascript/roman-numerals/roman.js
+++ javascript/roman-numerals/roman.js
@@ -4,14 +4,30 @@
 
 function arabicToRomanNumeral (num) {
   let result = "";
-  let number = num;
+  const number = num;
 
-  while (number <= 10) {
-    if (number == 10) {
-      result += "X";
-      number -= 10;
-    }
+  if (number === 1){
+    result += "I";
+  } else if (number === 2) {
+    result += "II";
+  } else if (number === 3) {
+    result += "III";
+  } else if (number === 4) {
+    result += "IV";
+  } else if (number === 5) {
+    result += "V";
+  } else if (number === 6) {
+    result += "VI";
+  } else if (number === 7) {
+    result += "VII";
+  } else if (number === 8) {
+    result += "VIII";
+  } else if (number === 9) {
+    result += "IX";
+  } else if (number === 10) {
+    result += "X";
   }
+
 console.log(number);
 
 return result;
@@ -22,7 +38,7 @@ function testArabicToRomanNumeral (number, expected) {
   const actual = arabicToRomanNumeral(number);
 
   if (actual === expected){
-    console.log("true");
+    console.log("Passing Test");
   } else {
     console.log("ERROR");
   }
@@ -30,3 +46,12 @@ function testArabicToRomanNumeral (number, expected) {
 
 
 testArabicToRomanNumeral(1, "I");
+testArabicToRomanNumeral(2, "II");
+testArabicToRomanNumeral(3, "III");
+testArabicToRomanNumeral(4, "IV");
+testArabicToRomanNumeral(5, "V");
+testArabicToRomanNumeral(6, "VI");
+testArabicToRomanNumeral(7, "VII");
+testArabicToRomanNumeral(8, "VIII");
+testArabicToRomanNumeral(9, "IX");
+testArabicToRomanNumeral(10, "X");
