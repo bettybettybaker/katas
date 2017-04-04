7ce534adb7878143dac9956b6f9c1a7af1110c12
Betty Baker <betty@heysparkbox.com>
2017-04-04 10:33:06 -0400

    Add 2 Rolls Test Case

--- javascript/bowling/bowling.js
+++ javascript/bowling/bowling.js
@@ -1,9 +1,22 @@
+/*eslint no-console: ["off"] */
+/*eslint one-var: ["error", "never"]*/
+/*eslint max-statements: ["error", 15, { "ignoreTopLevelFunctions": true }]*/
+/*eslint no-multiple-empty-lines: ["error", { "max": 4, "maxBOF": 1}]*/
+/*eslint max-len: ["error", 300]*/
+/*eslint sort-keys: ["off"]*/
+/*eslint no-trailing-spaces: ["error", { "skipBlankLines": true }]*/
+/*eslint max-statements: ["error", 20, { "ignoreTopLevelFunctions": true }]*/
+
 class Game {
-  roll (pinsDown) {
-    this.firstRoll = pinsDown;
+  roll (firstRoll, secondRoll) {
+    this.firstRoll = firstRoll;
+    this.secondRoll = secondRoll;
+    this.frame = this.firstRoll + this.secondRoll;
+    // console.log(this.frame);
   }
   score () {
-    return this.firstRoll;
+    console.log(this.frame);
+    return this.frame;
   }
 }
 
@@ -21,13 +34,20 @@ function assertBowlingScore (expected, actual) {
 const game = new Game();
 
 // Act
-const actual = game.roll(2);
+//const actual = game.roll(2);
 
 // Assert
-assertBowlingScore(2, actual);
+//assertBowlingScore(2, actual);
 
 /* TEST CASE: 2 Rolls - no spare or strike */
+// Arrange - See Single Roll Test Case
 
+// Act
+game.roll(1, 4);
+const actual = game.score();
+
+// Assert
+assertBowlingScore(5, actual);
 
 /* TEST CASE: 2 Rolls - no spare or strike */
 
